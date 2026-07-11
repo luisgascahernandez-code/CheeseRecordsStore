a39a810545c28d33c8e3f18f623a5743cf0b8a70
Luis_gaska <luisgascahernandez@gmail.com>
2026-07-11 12:53:46 -0600

    feat: Configurar conexión a PostgreSQL mediante Entity Framework para inventario

---
+++ CheeseRecordsStore/Data/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CheeseRecordsStore.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        // Esta es la misma cadena de conexión de tu appsettings.json
+        optionsBuilder.UseNpgsql("Host=localhost;Database=CheeseRecordsDB;Username=postgres;Password=admin123");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
