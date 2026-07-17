a80ddc30e094e25c267e38b9ce2129ac2b177e59
Luis_gaska <luisgascahernandez@gmail.com>
2026-07-17 17:25:17 -0600

    feat: Agregar clasificación de géneros a la base de datos y al formulario UI

--- CheeseRecordsStore/Models/Beat.cs
+++ CheeseRecordsStore/Models/Beat.cs
@@ -1,9 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace CheeseRecordsStore.Models
+
 {
+    public enum GeneroMusical
+    {
+        Cumbia,
+        Reggeaton,
+        HipHop,
+        Trap,
+        Rap,
+        Corridos,
+        Dembow,
+        RnB,
+        Pop
+    }
     public class Beat
     {
+        public GeneroMusical Genero { get; set; }
+
         [Key] // Esto le dice a PostgreSQL que este es el ID principal
         public int Id { get; set; }
 
